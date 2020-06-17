3697e0b507253e15a90d4690e086f5f182faba3c
Joshua Gomberoff <joshuagomberoff@gmail.com>
2020-06-17 22:57:15 +0000

    Add event listener for form submit, ajax post and get requests

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,40 +6,16 @@
 
 $(document).ready(function() {
 
-  // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
-
   const renderTweets = function(tweets) {
+    // clears the container before adding all of the tweets
+    $('#tweets-container').empty();
     // loops through tweets
     for (const tweet of tweets) {
 
       // calls createTweetElement for each tweet
       const $tweet = createTweetElement(tweet)
       // takes return value and appends it to the tweets container
-      $('#tweets-container').append($tweet);
+      $('#tweets-container').prepend($tweet);
     }
   };
 
@@ -64,5 +40,33 @@ const data = [
     return markup;
   };
 
-  renderTweets(data);
+
+  const loadTweets = function() {
+    $.getJSON('/tweets')
+      .then(function(tweets) {
+        // calls renderTweets function passing all tweets from /tweets
+        renderTweets(tweets);
+      })
+  };
+
+
+  $('#new-tweet').on('submit', function(event) {
+    event.preventDefault();
+
+    // checking for correct character length
+    if ($('#tweet-text').val().length > 140 || !$('#tweet-text').val()) {
+      alert('Tweet not within character limit');
+
+    } else {
+    const data = $(this).serialize();
+
+    $.post('/tweets', data)
+      .then(function(tweetData) {
+
+        loadTweets();
+      })
+    }
+  });
+
+  loadTweets();
 });
